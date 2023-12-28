1a1ede8a3fd6b0c8a751241d243800b5fe9ce9a9
Thanh Le <nhatthanhh1993@gmail.com>
2023-12-28 17:50:36 +0200

    create store with Zustand

---
+++ apps/client/src/store/index.ts
@@ -0,0 +1,100 @@
+// store.ts
+import { Sheet, SheetCreationInput } from '@client/trpc';
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
+
+interface StoreState {
+  sheets: Sheet[];
+  updateSheets: (sheets: Sheet[]) => void;
+
+  newSheet: Sheet | null;
+  updateNewSheet: (sheet: SheetCreationInput | null) => void;
+
+  selectedSheet: Sheet | null;
+  updateSelectedSheet: (sheet: Sheet | null) => void;
+
+  viewMode: 'CREATION' | 'VIEWING' | null;
+  updateViewMode: (mode: 'CREATION' | 'VIEWING' | null) => void;
+}
+
+export const useAppStore = create<StoreState>()(
+  immer(
+    devtools(
+      persist(
+        (set) => ({
+          sheets: [],
+          selectedSheet: null,
+          newSheet: null,
+          viewMode: null,
+          updateSheets: (sheets: Sheet[]) =>
+            set(
+              () => ({
+                sheets,
+              }),
+              false,
+              'updateSheets',
+            ),
+          updateSelectedSheet: (sheet: Sheet | null) =>
+            set(
+              () => {
+                if (sheet === null) return { selectedSheet: null };
+                return {
+                  selectedSheet: sheet,
+                  viewMode: 'VIEWING',
+                  newSheet: null,
+                };
+              },
+              false,
+              'updateSelectedSheet',
+            ),
+          updateNewSheet: (sheet: SheetCreationInput | null) =>
+            set(
+              () => {
+                if (sheet === null) return { newSheet: null };
+                return {
+                  newSheet: sheet,
+                  viewMode: 'CREATION',
+                  selectedSheet: null,
+                };
+              },
+              false,
+              'updateNewSheet',
+            ),
+          updateViewMode: (mode: 'CREATION' | 'VIEWING' | null) =>
+            set(
+              () => {
+                // if (mode === null)
+                //   return {
+                //     viewMode: null,
+                //     selectedSheet: null,
+                //     newSheet: null,
+                //   };
+                // if (mode === 'CREATION')
+                //   return {
+                //     viewMode: 'CREATION',
+                //     selectedSheet: null,
+                //   };
+                // if (mode === 'VIEWING')
+                //   return {
+                //     viewMode: 'VIEWING',
+                //     selectedSheet: null,
+                //     newSheet: null,
+                //   };
+
+                return {
+                  viewMode: mode,
+                };
+              },
+              false,
+              'updateViewMode',
+            ),
+        }),
+        {
+          name: 'excel-alike-editor',
+          partialize: () => ({}),
+        },
+      ),
+    ),
+  ),
+);
