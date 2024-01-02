906bfcd9b799c454d0c6bf44a9b62472dbde05f8
Thanh Le <nhatthanhh1993@gmail.com>
2024-01-02 13:35:44 +0200

    Refactor SheetView component to simplify code

--- apps/client/src/components/layout/SheetView.tsx
+++ apps/client/src/components/layout/SheetView.tsx
@@ -9,24 +9,7 @@ const SheetView: React.FC = () => {
 
   if (!viewMode) return <div className="w-full" />;
 
-  if (viewMode === 'CREATION')
-    return (
-      <Tabs
-        defaultValue="sheetInfoView"
-        className="w-full border-l border-white border-opacity-10 mt-4"
-      >
-        <TabsList className="m-8 mb-0">
-          <TabsTrigger value="sheetInfoView">Sheet Info</TabsTrigger>
-        </TabsList>
-        <Separator
-          orientation="horizontal"
-          className="h-[1px] bg-primary opacity-10"
-        />
-        <TabsContent value="sheetInfoView">
-          <SheetForm />
-        </TabsContent>
-      </Tabs>
-    );
+  if (viewMode === 'CREATION') return <SheetForm />;
 
   return (
     <Tabs
