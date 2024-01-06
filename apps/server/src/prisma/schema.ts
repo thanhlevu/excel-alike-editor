a72aa3f62d96f81c795d62d5a2d3e7cd5d890b42
Thanh Le <nhatthanhh1993@gmail.com>
2024-01-06 20:14:10 +0200

    Update validation rules for schema.ts

--- apps/server/src/prisma/schema.ts
+++ apps/server/src/prisma/schema.ts
@@ -2,43 +2,43 @@ import { z } from 'zod';
 
 export const CellSchema = z.object({
   sheetId: z.string().uuid(),
-  rowIndex: z.number(),
-  colIndex: z.number(),
+  rowIndex: z.number().min(0),
+  colIndex: z.number().min(0),
   value: z.string().nullable(),
 });
 
 export const MergedCellsSchema = z.object({
   sheetId: z.string().uuid(),
-  startRowIndex: z.number(),
-  endRowIndex: z.number(),
-  startColIndex: z.number(),
-  endColIndex: z.number(),
+  startRowIndex: z.number().min(0),
+  endRowIndex: z.number().min(0),
+  startColIndex: z.number().min(0),
+  endColIndex: z.number().min(0),
 });
 
 export const SheetSchema = z.object({
   sheetId: z.string().uuid(),
-  sheetName: z.string(),
+  sheetName: z.string().min(3),
   createdAt: z.date(),
   lastEditedAt: z.date(),
-  creatorName: z.string(),
-  creatorEmail: z.string(),
+  creatorName: z.string().min(3),
+  creatorEmail: z.string().email(),
   cells: z.array(CellSchema),
   mergedCells: z.array(MergedCellsSchema),
 });
 
 export const SheetCreationInput = z.object({
-  sheetName: z.string(),
-  creatorName: z.string(),
-  creatorEmail: z.string(),
+  sheetName: z.string().min(3),
+  creatorName: z.string().min(3),
+  creatorEmail: z.string().email(),
   cells: z.array(CellSchema.omit({ sheetId: true })),
   mergedCells: z.array(MergedCellsSchema.omit({ sheetId: true })),
 });
 
 export const SheetInfoUpdateInput = z.object({
   sheetId: z.string().uuid(),
-  sheetName: z.string(),
-  creatorName: z.string(),
-  creatorEmail: z.string(),
+  sheetName: z.string().min(3),
+  creatorName: z.string().min(3),
+  creatorEmail: z.string().email(),
 });
 
 export const SheetTableUpdateInput = z.object({
